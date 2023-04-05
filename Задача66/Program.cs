f416b655c5bfb9fc7995f76197308e24f94409a4
Vladislav <galdinsh77@mail.ru>
2023-04-05 12:01:16 +0500

    Задача66

---
+++ Задача66/Program.cs
@@ -0,0 +1,13 @@
+﻿int f(int a, int b)
+{
+  if (a == b)
+    return a;
+  return f(a, b - 1) + b;
+}
+
+Console.Clear();
+Console.Write("Введите начальное число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите конечное число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма всех элементов от {m} до {n} = {f(m, n)}");
