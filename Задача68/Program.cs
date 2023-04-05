3564dd16da30b459ad25444af4395e9e6cbfac96
Vladislav <galdinsh77@mail.ru>
2023-04-05 12:41:26 +0500

    Задача68

---
+++ Задача68/Program.cs
@@ -0,0 +1,15 @@
+﻿int f(int a, int b)
+{
+   if (a == 0)
+        return b + 1;
+    else if (b == 0 && a > 0)
+        return f(a - 1, 1);
+    else
+        return (f(a - 1, f(a, b - 1)));
+}
+Console.Clear();
+Console.Write("Введите начальное число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите конечное число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(m, n));
