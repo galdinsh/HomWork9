06b2d197c55425ff8c99d35bb6c905d842712572
Vladislav <galdinsh77@mail.ru>
2023-04-05 11:43:08 +0500

    Задача64

---
+++ Задача64/Program.cs
@@ -0,0 +1,12 @@
+﻿string f(int n)
+{
+  if (n == 1)
+    return "1 ";
+  return $"{n} " + f(n - 1);
+}
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(f(n));
